4f51152ab8e0ecd2c6dd16e88dc59b47728d68c2
Shayminifly <sego.denjoy@gmail.com>
2023-08-17 14:36:16 +0200

    summary and keywords

---
+++ src/gpt.py
@@ -0,0 +1,72 @@
+from langchain.llms import OpenAI
+import numpy as np
+import pandas as pd
+import json
+import time
+import os
+from langchain import PromptTemplate, LLMChain
+from langchain.llms import GPT4All
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.streaming_aiter import AsyncIteratorCallbackHandler
+
+from langchain.llms import GPT4All
+from functools import partial
+from typing import Any, List
+from langchain.callbacks.manager import AsyncCallbackManagerForLLMRun
+from langchain.llms.utils import enforce_stop_tokens
+
+# https://github.com/langchain-ai/langchain/issues/5210#issuecomment-1646581246
+class AGPT4All(GPT4All):
+    async def _acall(self, prompt: str, stop: List[str] | None = None, run_manager: AsyncCallbackManagerForLLMRun | None = None, **kwargs: Any) -> str:
+        text_callback = None
+        if run_manager:
+            text_callback = partial(run_manager.on_llm_new_token, verbose=self.verbose)
+        text = ""
+        params = {**self._default_params(), **kwargs}
+        for token in self.client.generate(prompt,streaming = True, **params):
+            if text_callback:
+                await text_callback(token)
+            text += token
+        if stop is not None:
+            text = enforce_stop_tokens(text, stop)
+        return text
+
+
+async def run_prediction(llm_chain: LLMChain, text: str, prompt_ask: str):
+    input_dict = {"text": text, "prompt_ask": prompt_ask}
+    return await llm_chain.arun(input_dict)
+
+def prepare_model(llm: AGPT4All):
+    prompt_template = """
+    I will give you an internet discussion as a text.
+    Text: \"{text}\"
+    You are a helpful, respectful and honest assistant. {prompt_ask} Do not use weird caracters or write a list with numbers.
+    """
+    # Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+    # If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+    
+    
+    prompt = PromptTemplate(template=prompt_template, input_variables=["text", "prompt_ask"])
+    
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    return llm_chain
+
+def launch_model(model_local_path: str):
+    # Callbacks support token-wise streaming
+    callbacks = [AsyncIteratorCallbackHandler()]
+    
+    # Verbose is required to pass to the callback manager
+    llm = AGPT4All(
+        model=model_local_path,
+        callbacks=callbacks,
+        n_threads=8,
+        n_predict=100,
+        max_tokens=500,
+        verbose=True
+    )
+    return llm
+
+async def run(model_local_path: str, prompt_ask: str, text: str):
+    llm = launch_model(model_local_path)
+    llm_chain = prepare_model(llm)
+    return await run_prediction(llm_chain, text, prompt_ask)
